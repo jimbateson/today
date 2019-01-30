d494b52e3ef5d7b655a3612a95582f70a29a4d9f
James Bateson <jim.bateson@outlook.com>
2019-01-30 17:08:59 +0000

    :zap: Started build!

---
+++ Window.js
@@ -0,0 +1,32 @@
+const { BrowserWindow } = require('electron');
+
+// The default window settings
+
+const defaultProps = {
+	height: 800,
+	show: false,
+	width: 500
+};
+
+class Window extends BrowserWindow {
+	constructor ({ file, ...windowSettings })
+	{
+
+		// Calls a new window with the above properties
+		// Our default ones and any custom passed?
+		super({ ...defaultProps, ...windowSettings });
+
+		// Load html file and open devtools?
+		// TODO: What does this mean?
+		this.loadFile(file);
+		this.webContents.openDevTools();
+
+		// Prevent flicker when ready to show
+		this.once('ready-to-show', () => {
+			this.show();
+		});
+
+	}
+}
+
+module.exports = Window
