b645a54cddbd0b4365eba059a42546e8c6254cad
James Bateson <jim.bateson@outlook.com>
2020-07-05 13:47:40 +0100

    feat: integrating Vue functionality with Electron

--- Datastore.js
+++ Datastore.js
@@ -10,56 +10,6 @@ class DataStore extends Store {
 		this.todos = this.get('todos') || [];
 
 	}
-
-	saveTodos()
-	{
-
-		// Save the todos to a JSON file
-		this.set('todos', this.todos);
-
-		return this;
-
-	}
-
-	updateTodos(todos)
-	{
-
-		this.set('todos', todos);
-
-		return this;
-
-	}
-
-	getTodos()
-	{
-
-		this.todos = this.get('todos') || [];
-
-		return this;
-
-	}
-
-	addTodo(todo)
-	{
-
-		// Merge todos array with the new todo
-		this.todos = [...this.todos, todo];
-
-		// Re-set the todos
-		return this.saveTodos();
-
-	}
-
-	deleteTodo(todo)
-	{
-
-		// Filter out the todo we want to delete
-		this.todos = this.todos.filter(t => t !== todo);
-
-		// Re-set the todos
-		return this.saveTodos();
-
-	}
 }
 
 module.exports = DataStore;
